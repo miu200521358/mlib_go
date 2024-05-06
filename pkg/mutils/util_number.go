fae521dd2a461b0f18d9cb86efc2df511d3a7153
miu <38824651+miu200521358@users.noreply.github.com>
2024-05-07 07:27:36 +0900

    float64用のソート追加

--- pkg/mutils/util_number.go
+++ pkg/mutils/util_number.go
@@ -101,13 +101,42 @@ func (x Float32Slice) Len() int { return len(x) }
 // This implementation of Less places NaN values before any others, by using:
 //
 //	x[i] < x[j] || (math.IsNaN(x[i]) && !math.IsNaN(x[j]))
-func (x Float32Slice) Less(i, j int) bool { return x[i] < x[j] || (isNaN(x[i]) && !isNaN(x[j])) }
+func (x Float32Slice) Less(i, j int) bool { return x[i] < x[j] || (isNaN32(x[i]) && !isNaN32(x[j])) }
 func (x Float32Slice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
 
-// isNaN is a copy of math.IsNaN to avoid a dependency on the math package.
-func isNaN(f float32) bool {
+// isNaN32 is a copy of math.IsNaN to avoid a dependency on the math package.
+func isNaN32(f float32) bool {
 	return f != f
 }
 
 // Sort is a convenience method: x.Sort() calls Sort(x).
 func (x Float32Slice) Sort() { sort.Sort(x) }
+
+func SearchFloat64s(a []float64, x float64) int {
+	return sort.Search(len(a), func(i int) bool { return a[i] >= x })
+}
+
+func SortFloat64s(x []float64) { sort.Sort(Float64Slice(x)) }
+
+// Float64Slice implements Interface for a []float64, sorting in increasing order,
+// with not-a-number (NaN) values ordered before other values.
+type Float64Slice []float64
+
+func (x Float64Slice) Len() int { return len(x) }
+
+// Less reports whether x[i] should be ordered before x[j], as required by the sort Interface.
+// Note that floating-point comparison by itself is not a transitive relation: it does not
+// report a consistent ordering for not-a-number (NaN) values.
+// This implementation of Less places NaN values before any others, by using:
+//
+//	x[i] < x[j] || (math.IsNaN(x[i]) && !math.IsNaN(x[j]))
+func (x Float64Slice) Less(i, j int) bool { return x[i] < x[j] || (isNaN64(x[i]) && !isNaN64(x[j])) }
+func (x Float64Slice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+// isNaN64 is a copy of math.IsNaN to avoid a dependency on the math package.
+func isNaN64(f float64) bool {
+	return f != f
+}
+
+// Sort is a convenience method: x.Sort() calls Sort(x).
+func (x Float64Slice) Sort() { sort.Sort(x) }
