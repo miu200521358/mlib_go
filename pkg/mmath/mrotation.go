61a89492181f66859cef2256bfbb2dd7bfbe44c9
miu <38824651+miu200521358@users.noreply.github.com>
2024-05-15 19:48:32 +0900

    遅延初期化

--- pkg/mmath/mrotation.go
+++ pkg/mmath/mrotation.go
@@ -3,19 +3,20 @@ package mmath
 import (
 	"fmt"
 	"math"
+	"reflect"
 )
 
 type MRotation struct {
-	radians    MVec3
-	degrees    MVec3
-	quaternion MQuaternion
+	radians    *MVec3
+	degrees    *MVec3
+	quaternion *MQuaternion
 }
 
 func NewRotation() *MRotation {
 	model := &MRotation{
-		radians:    NewMVec3(),
-		degrees:    NewMVec3(),
-		quaternion: NewMQuaternion(),
+		radians:    nil,
+		degrees:    nil,
+		quaternion: nil,
 	}
 	return model
 }
@@ -42,13 +43,17 @@ func NewRotationByQuaternion(vQuaternion *MQuaternion) *MRotation {
 }
 
 func (m *MRotation) GetQuaternion() *MQuaternion {
-	return &m.quaternion
+	if m.quaternion == nil {
+		qq := NewMQuaternion()
+		m.quaternion = &qq
+	}
+	return m.quaternion
 }
 
 func (m *MRotation) SetQuaternion(v *MQuaternion) {
-	m.quaternion = *v
-	m.radians = *v.ToRadians()
-	m.degrees = MVec3{
+	m.quaternion = v
+	m.radians = v.ToRadians()
+	m.degrees = &MVec3{
 		180.0 * m.radians.GetX() / math.Pi,
 		180.0 * m.radians.GetY() / math.Pi,
 		180.0 * m.radians.GetZ() / math.Pi,
@@ -56,50 +61,81 @@ func (m *MRotation) SetQuaternion(v *MQuaternion) {
 }
 
 func (m *MRotation) GetRadians() *MVec3 {
-	return &m.radians
+	if m.radians == nil {
+		v := NewMVec3()
+		m.radians = &v
+	}
+	return m.radians
 }
 
 func (m *MRotation) SetRadians(v *MVec3) {
-	m.radians = *v
-	m.degrees = MVec3{
+	m.radians = v
+	m.degrees = &MVec3{
 		180.0 * v.GetX() / math.Pi,
 		180.0 * v.GetY() / math.Pi,
 		180.0 * v.GetZ() / math.Pi,
 	}
-	m.quaternion = NewMQuaternionFromRadians(v.GetX(), v.GetY(), v.GetZ())
+	qq := NewMQuaternionFromRadians(v.GetX(), v.GetY(), v.GetZ())
+	m.quaternion = &qq
 }
 
 func (m *MRotation) GetDegrees() *MVec3 {
-	return &m.degrees
+	if m.degrees == nil {
+		v := NewMVec3()
+		m.degrees = &v
+	}
+	return m.degrees
 }
 
 func (m *MRotation) SetDegrees(v *MVec3) {
-	m.degrees = *v
-	m.radians = MVec3{
+	m.degrees = v
+	m.radians = &MVec3{
 		math.Pi * v.GetX() / 180.0,
 		math.Pi * v.GetY() / 180.0,
 		math.Pi * v.GetZ() / 180.0,
 	}
-	m.quaternion = NewMQuaternionFromRadians(m.radians.GetX(), m.radians.GetY(), m.radians.GetZ())
+	qq := NewMQuaternionFromRadians(m.radians.GetX(), m.radians.GetY(), m.radians.GetZ())
+	m.quaternion = &qq
 }
 
 // Copy
 func (rot *MRotation) Copy() *MRotation {
-	copied := &MRotation{
-		radians:    rot.radians.Copy(),
-		degrees:    rot.degrees.Copy(),
-		quaternion: rot.quaternion.Copy(),
+	copied := NewRotation()
+	if rot.radians != nil {
+		copied.radians = &MVec3{rot.radians.GetX(), rot.radians.GetY(), rot.radians.GetZ()}
+	}
+	if rot.degrees != nil {
+		copied.degrees = &MVec3{rot.degrees.GetX(), rot.degrees.GetY(), rot.degrees.GetZ()}
+	}
+	if rot.quaternion != nil {
+		quat := NewMQuaternionByValues(rot.quaternion.GetX(), rot.quaternion.GetY(), rot.quaternion.GetZ(), rot.quaternion.GetW())
+		copied.quaternion = &quat
 	}
 	return copied
 }
 
 // Mul
 func (rot *MRotation) Mul(v *MRotation) {
-	qq := rot.quaternion.Mul(&v.quaternion)
+	if rot.quaternion == nil {
+		qq := NewMQuaternion()
+		rot.quaternion = &qq
+	}
+	if v.quaternion == nil {
+		qq := NewMQuaternion()
+		v.quaternion = &qq
+	}
+	qq := rot.quaternion.Mul(v.quaternion)
 	rot.SetQuaternion(qq)
 }
 
 func (rot *MRotation) String() string {
+	stringOrNil := func(v fmt.Stringer) string {
+		if v == nil || reflect.ValueOf(v).IsNil() {
+			return "nil"
+		}
+		return v.String()
+	}
+
 	return fmt.Sprintf("degrees: %s, radians: %s, quat: %s",
-		rot.degrees.String(), rot.radians.String(), rot.quaternion.String())
+		stringOrNil(rot.degrees), stringOrNil(rot.radians), stringOrNil(rot.quaternion))
 }
