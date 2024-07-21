875449bad2f558eac7f85a5abfa33d0e0d0f737a
miu <38824651+miu200521358@users.noreply.github.com>
2024-07-21 22:38:18 +0900

    再生中に物理OFFしたときに描画が消えるのを修正

--- pkg/infrastructure/animation/animation_state.go
+++ pkg/infrastructure/animation/animation_state.go
@@ -152,7 +152,7 @@ func (animationState *AnimationState) DeformPhysics(physics mbt.IPhysics, appSta
 		}
 
 		if rigidBody.PhysicsType != pmx.PHYSICS_TYPE_DYNAMIC {
-			// ボーン追従剛体・物理＋ボーン位置もしくは強制更新の場合のみ剛体位置更新
+			// ボーン追従剛体・物理＋ボーン剛体
 			physics.UpdateTransform(animationState.ModelIndex(), rigidBodyBone,
 				animationState.vmdDeltas.Bones.Get(rigidBodyBone.Index()).FilledGlobalMatrix(), rigidBody)
 		}
@@ -160,22 +160,20 @@ func (animationState *AnimationState) DeformPhysics(physics mbt.IPhysics, appSta
 }
 
 func (animationState *AnimationState) DeformAfterPhysics(physics mbt.IPhysics, appState state.IAppState) {
-	if animationState.model == nil || !appState.IsEnabledPhysics() {
-		return
-	}
-
-	// 物理剛体位置を更新
-	for _, isAfterPhysics := range []bool{false, true} {
-		for _, bone := range animationState.model.Bones.LayerSortedBones[isAfterPhysics] {
-			if bone.Extend.RigidBody == nil || bone.Extend.RigidBody.PhysicsType == pmx.PHYSICS_TYPE_STATIC {
-				continue
-			}
-			bonePhysicsGlobalMatrix := physics.GetRigidBodyBoneMatrix(
-				animationState.ModelIndex(), bone.Extend.RigidBody)
-			if animationState.vmdDeltas.Bones != nil && bonePhysicsGlobalMatrix != nil {
-				bd := delta.NewBoneDeltaByGlobalMatrix(bone, int(appState.Frame()),
-					bonePhysicsGlobalMatrix, animationState.vmdDeltas.Bones.Get(bone.ParentIndex))
-				animationState.vmdDeltas.Bones.Update(bd)
+	if animationState.model != nil && appState.IsEnabledPhysics() {
+		// 物理剛体位置を更新
+		for _, isAfterPhysics := range []bool{false, true} {
+			for _, bone := range animationState.model.Bones.LayerSortedBones[isAfterPhysics] {
+				if bone.Extend.RigidBody == nil || bone.Extend.RigidBody.PhysicsType == pmx.PHYSICS_TYPE_STATIC {
+					continue
+				}
+				bonePhysicsGlobalMatrix := physics.GetRigidBodyBoneMatrix(
+					animationState.ModelIndex(), bone.Extend.RigidBody)
+				if animationState.vmdDeltas.Bones != nil && bonePhysicsGlobalMatrix != nil {
+					bd := delta.NewBoneDeltaByGlobalMatrix(bone, int(appState.Frame()),
+						bonePhysicsGlobalMatrix, animationState.vmdDeltas.Bones.Get(bone.ParentIndex))
+					animationState.vmdDeltas.Bones.Update(bd)
+				}
 			}
 		}
 	}
