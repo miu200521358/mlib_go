d1e8271c0020257862046353a1a4e0fa2c09b480
miu <38824651+miu200521358@users.noreply.github.com>
2025-03-18 15:31:51 +0900

    sharedが必要なボタンの追加

---
+++ pkg/interface/controller/widget/push_button.go
@@ -0,0 +1,63 @@
+package widget
+
+import (
+	"github.com/miu200521358/mlib_go/pkg/interface/controller"
+	"github.com/miu200521358/walk/pkg/declarative"
+	"github.com/miu200521358/walk/pkg/walk"
+)
+
+type MPushButton struct {
+	*walk.PushButton
+	window    *controller.ControlWindow
+	label     string
+	maxSize   declarative.Size
+	onClicked func(cw *controller.ControlWindow)
+	tooltip   string
+}
+
+func NewMPushButton() *MPushButton {
+	return new(MPushButton)
+}
+
+func (b *MPushButton) SetTooltip(tooltip string) {
+	b.tooltip = tooltip
+}
+
+func (b *MPushButton) SetLabel(label string) {
+	b.label = label
+}
+
+func (b *MPushButton) SetMaxSize(maxSize declarative.Size) {
+	b.maxSize = maxSize
+}
+
+func (b *MPushButton) SetOnClicked(onClicked func(cw *controller.ControlWindow)) {
+	b.onClicked = onClicked
+}
+
+func (b *MPushButton) EnabledInPlaying(enable bool) {
+	b.PushButton.SetEnabled(enable)
+}
+
+func (b *MPushButton) SetWindow(window *controller.ControlWindow) {
+	b.window = window
+}
+
+func (b *MPushButton) Widgets() declarative.Composite {
+	return declarative.Composite{
+		Layout: declarative.HBox{},
+		Children: []declarative.Widget{
+			declarative.PushButton{
+				AssignTo:    &b.PushButton,
+				Text:        b.label,
+				MaxSize:     b.maxSize,
+				ToolTipText: b.tooltip,
+				OnClicked: func() {
+					if b.onClicked != nil {
+						b.onClicked(b.window)
+					}
+				},
+			},
+		},
+	}
+}
