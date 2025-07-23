d475c0539c7e2f7495d7972056c481356b18be62
miu <38824651+miu200521358@users.noreply.github.com>
2025-07-24 04:13:31 +0900

    ConsoleViewの行数制限を追加し、行数が上限を超えた場合に古い行を削除する処理を実装。また、ログチャンネルの非ブロッキング送信を改善。

--- pkg/interface/controller/console_view.go
+++ pkg/interface/controller/console_view.go
@@ -5,6 +5,7 @@ package controller
 
 import (
 	"bytes"
+	"strings"
 	"sync"
 	"syscall"
 	"unsafe"
@@ -16,19 +17,21 @@ import (
 // ConsoleView は、walk.WidgetBaseを埋め込んでカスタムウィジェットを作成します。
 type ConsoleView struct {
 	walk.WidgetBase
-	Console *walk.TextEdit
-	logChan chan string
-	mutex   sync.Mutex
+	Console   *walk.TextEdit
+	logChan   chan string
+	mutex     sync.Mutex
+	lineCount int // 現在の行数をカウント
 }
 
 const (
 	ConsoleViewClass = "ConsoleView Class"
 	TEM_APPENDTEXT   = win.WM_USER + 6
+	MaxLines         = 10000 // 最大行数
 )
 
 func NewConsoleView(parent walk.Container, minWidth int, minHeight int) (*ConsoleView, error) {
-	lc := make(chan string, 512)
-	cv := &ConsoleView{logChan: lc}
+	lc := make(chan string, 10000) // バッファサイズを10000に増加
+	cv := &ConsoleView{logChan: lc, lineCount: 0}
 
 	if err := walk.InitWidget(
 		cv,
@@ -67,19 +70,66 @@ func (controlView *ConsoleView) textLength() int {
 }
 
 func (controlView *ConsoleView) AppendText(value string) {
+	// 行数をカウント
+	controlView.lineCount += strings.Count(value, "\r\n")
+
 	textLength := controlView.textLength()
 	controlView.setTextSelection(textLength, textLength)
 	uv, err := syscall.UTF16PtrFromString(value)
 	if err == nil {
 		controlView.Console.SendMessage(win.EM_REPLACESEL, 0, uintptr(unsafe.Pointer(uv)))
 	}
+
+	// 行数制限のチェック（100行ごとに実行してパフォーマンスを保つ）
+	if controlView.lineCount%100 == 0 && controlView.lineCount > MaxLines {
+		controlView.trimLines()
+	}
 }
 func (controlView *ConsoleView) PostAppendText(value string) {
 	controlView.mutex.Lock()
 	defer controlView.mutex.Unlock()
 
-	controlView.logChan <- value
-	win.PostMessage(controlView.Handle(), TEM_APPENDTEXT, 0, 0)
+	// 非ブロッキング送信：チャンネルがフルの場合は古いメッセージを破棄
+	select {
+	case controlView.logChan <- value:
+		// 正常に送信できた場合
+		win.PostMessage(controlView.Handle(), TEM_APPENDTEXT, 0, 0)
+	default:
+		// チャンネルがフルの場合、古いメッセージを1つ破棄して新しいメッセージを追加
+		select {
+		case <-controlView.logChan:
+			// 古いメッセージを破棄
+		default:
+		}
+		// 新しいメッセージを送信
+		select {
+		case controlView.logChan <- value:
+			win.PostMessage(controlView.Handle(), TEM_APPENDTEXT, 0, 0)
+		default:
+			// それでも送信できない場合は諦める
+		}
+	}
+}
+
+// trimLines は、行数が上限を超えた場合に古い行を削除します
+func (controlView *ConsoleView) trimLines() {
+	text := controlView.Console.Text()
+	lines := strings.Split(text, "\r\n")
+
+	if len(lines) > MaxLines {
+		// 先頭から削除する行数を計算
+		linesToRemove := len(lines) - MaxLines
+		// 新しいテキストを作成
+		newText := strings.Join(lines[linesToRemove:], "\r\n")
+
+		// テキストを更新
+		controlView.Console.SetText(newText)
+		controlView.lineCount = MaxLines
+
+		// カーソルを最後に移動
+		textLength := controlView.textLength()
+		controlView.setTextSelection(textLength, textLength)
+	}
 }
 
 func (controlView *ConsoleView) WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
@@ -104,10 +154,9 @@ func (controlView *ConsoleView) WndProc(hwnd win.HWND, msg uint32, wParam, lPara
 
 // Write はio.Writerインターフェースを実装し、logの出力をConsoleViewにリダイレクトします。
 func (controlView *ConsoleView) Write(p []byte) (n int, err error) {
-
 	// 改行が文字列内にある場合、コンソール内で改行が行われるよう置換する
 	p = bytes.ReplaceAll(p, []byte("\n"), []byte("\r\n"))
 	controlView.PostAppendText(string(p))
 
-	return n, nil
+	return len(p), nil
 }
