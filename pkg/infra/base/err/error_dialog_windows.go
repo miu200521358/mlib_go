d19bccb0a08a707c20b2c04ae305193d680e7705
miu <38824651+miu200521358@users.noreply.github.com>
2026-01-19 23:44:00 +0900

    refactor: launch.jsonのプログラムパスを修正し、エラーダイアログの表示処理を改善

--- pkg/infra/base/err/error_dialog_windows.go
+++ pkg/infra/base/err/error_dialog_windows.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/miu200521358/mlib_go/pkg/infra/base/i18n"
 	"github.com/miu200521358/mlib_go/pkg/shared/base/config"
+	"github.com/miu200521358/walk/pkg/declarative"
 	"github.com/miu200521358/walk/pkg/walk"
 )
 
@@ -26,10 +27,85 @@ func ShowFatalErrorDialog(appConfig *config.AppConfig, err error) bool {
 // showErrorDialog はエラーダイアログの表示を行う。
 func showErrorDialog(appConfig *config.AppConfig, err error, title string, header string, terminate bool) bool {
 	message := replaceAppInfo(header, appConfig)
+	errText := ""
 	if err != nil {
-		message += "\n\n" + err.Error()
+		errText = err.Error()
 	}
-	walk.MsgBox(nil, title, message, walk.MsgBoxIconError|walk.MsgBoxOK)
+	text := message
+	if errText != "" {
+		text += "\n\n" + errText
+	}
+	// ToolTip追加で失敗する環境があるため、エラーダイアログ生成中だけ抑止する。
+	prevEnv, hasEnv := os.LookupEnv("Env")
+	if setErr := os.Setenv("Env", "debug"); setErr == nil {
+		defer func() {
+			if hasEnv {
+				_ = os.Setenv("Env", prevEnv)
+			} else {
+				_ = os.Unsetenv("Env")
+			}
+		}()
+	}
+	closeText := i18n.T("エラーダイアログを閉じる")
+	if terminate {
+		closeText = i18n.T("アプリを終了")
+	}
+	var mw *walk.MainWindow
+	var errView *walk.TextEdit
+	if _, dialogErr := (declarative.MainWindow{
+		AssignTo: &mw,
+		Title:    title,
+		Size:     declarative.Size{Width: 680, Height: 520},
+		MinSize:  declarative.Size{Width: 680, Height: 520},
+		MaxSize:  declarative.Size{Width: 1200, Height: 900},
+		Layout:   declarative.VBox{},
+		Children: []declarative.Widget{
+			declarative.TextLabel{
+				Text: replaceAppInfo(header, appConfig),
+			},
+			declarative.TextEdit{
+				Text:     strings.ReplaceAll(text, "\n", "\r\n"),
+				ReadOnly: true,
+				AssignTo: &errView,
+				VScroll:  true,
+				HScroll:  true,
+			},
+			declarative.Composite{
+				Layout: declarative.HBox{},
+				Children: []declarative.Widget{
+					declarative.PushButton{
+						Text: i18n.T("コミュニティ報告"),
+						OnClicked: func() {
+							if errView == nil {
+								return
+							}
+							if copyErr := walk.Clipboard().SetText(errView.Text()); copyErr != nil {
+								walk.MsgBox(nil, i18n.T("クリップボードコピー失敗"), copyErr.Error(), walk.MsgBoxIconError)
+							}
+						},
+					},
+					declarative.HSpacer{},
+					declarative.PushButton{
+						Text: closeText,
+						OnClicked: func() {
+							if terminate {
+								os.Exit(1)
+							}
+							if mw != nil {
+								mw.Close()
+							}
+						},
+					},
+				},
+			},
+		},
+	}).Run(); dialogErr != nil {
+		walk.MsgBox(nil, i18n.T("エラーダイアログ起動失敗"), dialogErr.Error(), walk.MsgBoxIconError)
+		if terminate {
+			os.Exit(1)
+		}
+		return false
+	}
 	if terminate {
 		os.Exit(1)
 	}
